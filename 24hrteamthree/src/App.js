e0546aa13c20f3f750a5a519861fac21567690b6
laurencobbs <lcobbs1407@gmail.com>
2021-10-26 09:40:46 -0400

    push update

---
+++ 24hrteamthree/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import OpenWeather from '../src/components/concepts/OpenWeather/OpenWeather';
+
+
+function App() {
+  return (
+    <div className="App">
+      <OpenWeather />
+    </div>
+  );
+}
+
+export default App;
